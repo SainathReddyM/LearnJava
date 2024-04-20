c48d921fa619ab9da66ec2ff347dcf60ec4faca4
Sainath <sainath@thoughtclan.com>
2024-04-20 23:52:36 +0530

    commit 20/04/2024

--- JavaBasics/src/learning/java/com/Main.java
+++ JavaBasics/src/learning/java/com/Main.java
@@ -38,5 +38,32 @@ public class Main {
      *        It has bytecode loader, verifier and interpreter.
      *        Can convert byte code to machine specific code.
      *
+     * Advantages of the Java bytecode model:
+     * 1) Write once, run anywhere.
+     * 2) Security-  JVM acts as a sandbox.
+     *
+     * Doesn't it make things slow as JVM is interpreting the code instead of running machine level code directly on the machine?
+     * Java does few things to optimise processing. One of them is Just-in-time(JIT) compiler of java.
+     * When JVM detects a piece of code that has to be run multiple number of times, it will convert that peice of code into machine
+     * level code and run it to optimise performance.
+     *
+     * There are 3 ways to run java bytecode
+     * 1) Interpret the intermediate bytecode directly.
+     * 2) Just before executing, compile to native code (JIT compiler)
+     * 3) Convert intermediate code fully to native code before executing.(AOT - Ahead of time compilation). This is available since
+     *    java-9 only.
+     *
+     * 5 goals java was designed to meet
+     * 1) It must be simple, object-oriented and familiar.
+     * 2) It must be robust and secure.
+     * 3) It must be architecture-neutral and portable.
+     * 4) It must execute with high performance.
+     * 5) It must be interpreted, threaded, and dynamic.
+     *
+     * Installing JDK
+     * Best way to install jdk is from openjdk.org. Other versions like the oracle has licensing requirements.
+     *
+     *
+     * sss
      */
 }
